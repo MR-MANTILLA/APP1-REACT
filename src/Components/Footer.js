5cf2a49dc7f68540248e2065ec41d1b23c386c69
Didier Mantilla Gelvez <80107743+MR-MANTILLA@users.noreply.github.com>
2021-04-20 09:53:24 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../App.css"
 
-
+onst anio = new Date().getFullYear();
 class footer extends React.Component{
   render() {
     return(
     <footer class="Footer">
       <div>
-        <p>&copy;iser 2021-1</p>
+        <p>&copy;iser {anio}-1</p>
       </div>
     </footer>
     );
